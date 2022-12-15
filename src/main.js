583f5639da448ab764daed2729885783eae96a58
SergViktKobzev <serg-vikt.kobzev@mail.ru>
2022-12-15 23:25:14 +0300

    1.7 Добавил презентер доски

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import {render} from './render.js';
 import FilterView from './view/filter-view.js';
+import BoardPresenter from './presenter/board-presenter.js';
 
-const filterContainerElement = document.querySelector('.trip-controls__filters');
+const headerContainerElement = document.querySelector('.trip-controls__filters');
+const boardContainerElement = document.querySelector('.trip-events');
 
-render(new FilterView(), filterContainerElement);
+const boardPresenter = new BoardPresenter({boardContainer: boardContainerElement});
+
+render(new FilterView(), headerContainerElement);
+boardPresenter.init();
+
